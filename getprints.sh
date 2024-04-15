84f161a5b893a0fb065a0c756acfd1d5189ad602
Cyber Anonymous <cyberx99@disroot.org>
2024-04-15 13:42:56 -0300

    first commit

---
+++ getprints.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+### change this line to your user id if you want to use the script on your Steam Deck, you can get your user id from the website: https://steamdb.info/
+USERID=487786334
+###
+
+for i in $(find /home/deck/.steam/steam/userdata/$USERID/ -name "*.jpg" | grep screenshot)
+do
+cp -v "$i" ss/
+done
